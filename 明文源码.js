6bfb00562c669147c1f71bd42770247590006b28
wasilvsi <66351998@qq.com>
2024-12-20 14:37:37 +0100

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -1,14 +1,14 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
+let userID = '888a92d4-72e8-4c01-b573-36e111bee0ef';
 let proxyIP = '';
 let sub = '';
 let subConverter = 'SUBAPI.fxxk.dedyn.io';
 let subConfig = "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Mini_MultiMode.ini";
 let subProtocol = 'https';
 let subEmoji = 'true';
-let socks5Address = '';
+let socks5Address = 'wa:21@31.186.87.211:9921';
 let parsedSocks5Address = {}; 
 let enableSocks = false;
 
